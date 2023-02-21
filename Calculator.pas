c9c97edde08117e30f61fd79abcc3a5fb19ec9da
Egor Tunekov <69630128+olovink@users.noreply.github.com>
2023-02-21 18:19:02 +0700

    Create Calculator.pas

---
+++ Calculator.pas
@@ -0,0 +1,12 @@
+﻿program Calculator;
+
+var a, b:integer;
+
+begin
+
+  writeln('Введите число a:'); readln(a);
+  writeln('Введите число b:'); readln(b);
+ 
+  writeln('Сумма чисел ', a ,' + ', b ,' = ', a + b);
+
+end.
